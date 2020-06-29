a239e88c9ecd99c2988daf353de34f218fb3faa9
Tarek, Hamid <hamidtarek3@gmail.com>
2020-06-28 22:57:56 -0400

    submitting m1 s5

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -3,27 +3,25 @@ package com.techelevator;
 import java.util.Scanner;
 
 public class Fibonacci {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter the Fibonacci number");
+		System.out.println("Please enter the Fibonacci number: ");
 		String value = scanner.nextLine();
 		int fibonacciNumber = Integer.parseInt(value);
 		int temporaryVariable = 0;
 		int previousTerm = 0;
 		int sum = 1;
 		System.out.print("0, 1");
-		while (sum < fibonacciNumber){
+		while (sum < fibonacciNumber) {
 			temporaryVariable = sum;
 			sum += previousTerm;
 			previousTerm = temporaryVariable;
-			if (sum < fibonacciNumber){
+			if (sum < fibonacciNumber) {
 				System.out.print(", " + sum);
 			} else {
 				break;
 			}
-
 		}
 	}
-
 }
+
