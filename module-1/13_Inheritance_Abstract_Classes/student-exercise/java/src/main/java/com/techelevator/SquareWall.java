23bddf8a4f9aa69ab08fda649d1bcf9f08b5719f
Tarek, Hamid <hamidtarek3@gmail.com>
2020-07-09 13:58:38 -0400

    submitting w3d4

---
+++ module-1/13_Inheritance_Abstract_Classes/student-exercise/java/src/main/java/com/techelevator/SquareWall.java
@@ -0,0 +1,19 @@
+package com.techelevator;
+
+public class SquareWall extends RectangleWall {
+
+    private int sideLength;
+
+    public SquareWall(String name, String color, int sideLength){
+        super(name, color, sideLength, sideLength);
+        this.sideLength = sideLength;
+    }
+
+    public int getSideLength() {
+        return sideLength;
+    }
+
+    public String toString(){
+        return getName() + " (" + sideLength + "x" + sideLength + ") square";
+    }
+}
---
+++ module-1/13_Inheritance_Abstract_Classes/student-exercise/java/src/main/java/com/techelevator/Wall.java
@@ -0,0 +1,30 @@
+package com.techelevator;
+
+public abstract class Wall {
+
+    private String name;
+    private String color;
+
+    public Wall (String name, String color){
+        this.name = name;
+        this.color = color;
+    };
+
+    public abstract int getArea();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
