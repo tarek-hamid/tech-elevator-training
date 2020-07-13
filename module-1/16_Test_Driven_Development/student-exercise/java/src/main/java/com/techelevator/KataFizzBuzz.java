4ae96641079dd109290620225aca084d2a5e03b0
Tarek, Hamid <hamidtarek3@gmail.com>
2020-07-13 15:42:25 -0400

    test isdivisiblebythree

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -2,4 +2,14 @@ package com.techelevator;
 
 public class KataFizzBuzz {
 
+    private int num;
+
+    public KataFizzBuzz() {
+    }
+
+    public String fizzBuzz(int num){
+        return null;
+    }
+
+
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -7,4 +7,16 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class KataFizzBuzzTest {
 
+    private KataFizzBuzz test;
+
+    @Before
+    public void setUp() throws Exception {
+        test = new KataFizzBuzz();
+    }
+
+    @Test
+    public void isDivisibleByThree() {
+        assertEquals("Fizz.", test.fizzBuzz(9));
+    }
+
 }
