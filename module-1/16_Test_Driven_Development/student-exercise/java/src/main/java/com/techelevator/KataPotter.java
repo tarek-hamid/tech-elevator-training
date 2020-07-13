0fe49a823e8f1b28fe7d6e94667137432cbbc711
Tarek, Hamid <hamidtarek3@gmail.com>
2020-07-13 17:15:37 -0400

    eachBookCosts8

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPotter.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPotter.java
@@ -1,5 +1,20 @@
 package com.techelevator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KataPotter {
+
+    // Methodology : evaluate all possibilities and store in Map
+
+    private int[] books;
+
+    public KataPotter() {
+    }
+
+    public int getCost(int[] books){
+        this.books = books;
+        return 0;
+    }
 
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
@@ -7,4 +7,15 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class KataPotterTest {
 
+    private KataPotter test;
+
+    @Before
+    public void setUp() throws Exception {
+        test = new KataPotter();
+    }
+
+    @Test
+    public void eachBookCostsEight() {
+        assertEquals(8, test.getCost(new int[]{1, 1}));
+    }
 }
