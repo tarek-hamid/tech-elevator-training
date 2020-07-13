cee04070c7e93eb88bff9519844d8c4bc8bd28cd
Tarek, Hamid <hamidtarek3@gmail.com>
2020-07-13 15:47:34 -0400

    test isdivisiblebyfive

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -10,8 +10,8 @@ public class KataFizzBuzz {
     public String fizzBuzz(int num){
         if (num % 3 == 0){
             return "Fizz.";
-//        } else if (num % 5 == 0) {
-//            return "Buzz.";
+        } else if (num % 5 == 0) {
+            return "Buzz.";
         }
         return null;
     }
