a8bfbc96af099b52992a6a50e1cd1be298f1ba96
Tarek, Hamid <hamidtarek3@gmail.com>
2020-07-14 14:54:45 -0400

    Submitting finished Potter

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPotter.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPotter.java
@@ -10,37 +10,40 @@ public class KataPotter {
     public KataPotter() {
     }
 
-    public String getCost(int[] books){
+    public String getCost(int[] books) {
 
         int[] noMoreBooks = {0, 0, 0, 0, 0};
-        int[] tempArray = books;
+        int[] tempArray = books.clone();
+        int[] tempArray2 = books.clone();
 
         DecimalFormat df = new DecimalFormat("0.00");
         double runningCostFiveSetMax = 0;
         double runningCostFourSetMax = 0;
         int bookCount = 0;
+        int bookCount2 = 0;
 
 
         // For loop using maximum set 5
         while (!Arrays.equals(tempArray, noMoreBooks)) {
-            for(int i = 0; i < tempArray.length; i++){
-                if (tempArray[i] > 0){
-                    bookCount += 1;
-                    tempArray[i] -= 1;
+            for (int i = 0; i < tempArray.length; i++) {
+                if (tempArray[i] != 0) {
+                    bookCount++;
+                    tempArray[i]--;
+                }
+                if (bookCount == 5) {
+                    runningCostFiveSetMax += (8 * 5 * 0.75);
+                    bookCount = 0;
                 }
             }
 
-            if (bookCount == 5) {
-                runningCostFiveSetMax += (8 * 5 * 0.75);
-                bookCount = 0;
-            } else if (bookCount == 4) {
+            if (bookCount == 4) {
                 runningCostFiveSetMax += (8 * 4 * 0.80);
                 bookCount = 0;
             } else if (bookCount == 3) {
-                runningCostFiveSetMax += (8 * 3 * 0.85);
+                runningCostFiveSetMax += (8 * 3 * 0.90);
                 bookCount = 0;
             } else if (bookCount == 2) {
-                runningCostFiveSetMax += (8 * 2 * 0.90);
+                runningCostFiveSetMax += (8 * 2 * 0.95);
                 bookCount = 0;
             } else if (bookCount == 1) {
                 runningCostFiveSetMax += 8;
@@ -48,8 +51,31 @@ public class KataPotter {
             }
         }
 
-
-       return df.format(runningCostFiveSetMax);
+        // For loop using max set 4
+        while (!Arrays.equals(tempArray2, noMoreBooks)) {
+            for (int i = 0; i < tempArray2.length; i++) {
+                if (tempArray2[i] != 0) {
+                    bookCount2++;
+                    tempArray2[i]--;
+                }
+                if (bookCount2 == 4) {
+                    runningCostFourSetMax += (8 * 4 * 0.80);
+                    bookCount2 = 0;
+                }
+            }
+        }
+            if (bookCount2 == 3) {
+                runningCostFourSetMax += (8 * 3 * 0.90);
+                bookCount = 0;
+            } else if (bookCount2 == 2) {
+                runningCostFourSetMax += (8 * 2 * 0.95);
+                bookCount = 0;
+            } else if (bookCount2 == 1) {
+                runningCostFourSetMax += 8;
+                bookCount = 0;
+            }
+            String bestPrice = df.format(Math.min(runningCostFiveSetMax, runningCostFourSetMax));
+            return bestPrice;
     }
-
 }
+
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
@@ -14,21 +14,27 @@ public class KataPotterTest {
         test = new KataPotter();
     }
 
-    @Test
-    public void totalPriceWithFiveSetMax() {
-        assertEquals("30.00", test.getCost(new int[]{1, 1, 1, 1, 1}));
-        assertEquals("25.60", test.getCost(new int[]{1, 1, 1, 1, 0}));
-        assertEquals("20.40", test.getCost(new int[]{1, 1, 1, 0, 0}));
-        assertEquals("14.40", test.getCost(new int[]{1, 1, 0, 0, 0}));
-        assertEquals("8.00", test.getCost(new int[]{1, 0, 0, 0, 0}));
-    }
+//
+//    @Test
+//    public void totalPriceWithFiveSetMax() {
+//        assertEquals("51.60", test.getCost(new int[]{2, 2, 2, 1, 1}));
+//    }
+
+//    @Test
+//    public void totalPriceWithFourSetMax() {
+//        assertEquals("33.60", test.getCost(new int[]{1, 1, 1, 1, 1}));
+//        assertEquals("25.60", test.getCost(new int[]{1, 1, 1, 1, 0}));
+//        assertEquals("20.40", test.getCost(new int[]{1, 1, 1, 0, 0}));
+//        assertEquals("14.40", test.getCost(new int[]{1, 1, 0, 0, 0}));
+//        assertEquals("8.00", test.getCost(new int[]{1, 0, 0, 0, 0}));
+//    }
 
     @Test
-    public void totalPriceWithFourSetMax() {
+    public void bestPrice() {
+        assertEquals("51.20",test.getCost(new int[] {2, 2, 2, 1, 1}));
+        assertEquals("21.60",test.getCost(new int[]{1, 1, 1, 0, 0}));
         assertEquals("30.00", test.getCost(new int[]{1, 1, 1, 1, 1}));
-        assertEquals("25.60", test.getCost(new int[]{1, 1, 1, 1, 0}));
-        assertEquals("20.40", test.getCost(new int[]{1, 1, 1, 0, 0}));
-        assertEquals("14.40", test.getCost(new int[]{1, 1, 0, 0, 0}));
+        assertEquals("68.00", test.getCost(new int[]{2, 3, 2, 2, 2}));
         assertEquals("8.00", test.getCost(new int[]{1, 0, 0, 0, 0}));
     }
 }
