c0319e0a23f46cf9dd13686cec776ca3f02e715e
Tarek, Hamid <hamidtarek3@gmail.com>
2020-07-15 13:50:12 -0400

    updated file i/o part 2

--- module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
@@ -12,23 +12,19 @@ public class FizzWriter {
 		String outputString = "";
 
 		try(PrintWriter dataOutput = new PrintWriter(dataFile)){
-			for(int i = 0; i < 301; i++){
+			for(int i = 1; i < 301; i++){
 				if (i % 15 == 0){
 					outputString = "FizzBuzz.";
-				} else if (i % 3 == 0) {
+				} else if (i % 3 == 0 || Integer.toString(i).contains("3")) {
 					outputString = "Fizz.";
-				} else if (i % 5 == 0) {
-					outputString = "Buzz.";
-				} else if (Integer.toString(i).contains("3")) {
-					outputString = "Fizz.";
-				} else if (Integer.toString(i).contains("5")) {
+				} else if (i % 5 == 0 || Integer.toString(i).contains("5")) {
 					outputString = "Buzz.";
 				} else {
 					outputString = Integer.toString(i);
 				}
 				dataOutput.println(outputString);
 			}
-			System.out.println("Fizzbuzz.txt has been created.");
+			System.out.println("FizzBuzz.txt has been created.");
 		} catch (FileNotFoundException e) {
 			System.out.println("");
 			System.exit(1);
